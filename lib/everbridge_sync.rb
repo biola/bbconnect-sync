32374f58d4f556dd1ebf957c821b8d6056831dec
Michael Stephens <michael.a.stephens@biola.edu>
2013-11-19 16:40:15 -0800

    Added redis server information to Settings

--- lib/everbridge_sync.rb
+++ lib/everbridge_sync.rb
@@ -12,6 +12,14 @@ module EverbridgeSync
       delivery_method Settings.email.delivery_method, address: Settings.email.options
     end
 
+    Sidekiq.configure_server do |config|
+      config.redis = { url: Settings.redis.url, namespace: 'everbridge-sync' }
+    end
+
+    Sidekiq.configure_client do |config|
+      config.redis = { url: Settings.redis.url, namespace: 'everbridge-sync' }
+    end
+
     require './lib/everbridge'
     require './lib/everbridge_sync/email'
     require './lib/everbridge_sync/email_log'
