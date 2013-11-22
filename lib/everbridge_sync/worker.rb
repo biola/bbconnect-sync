ac3b125e656b2fc89a9a8058f24c34253effe15a
Michael Stephens <michael.a.stephens@biola.edu>
2013-11-22 09:02:04 -0800

    Only run a job once

--- lib/everbridge_sync/worker.rb
+++ lib/everbridge_sync/worker.rb
@@ -9,6 +9,10 @@ module EverbridgeSync
     recurrence do
       daily.hour_of_day(Settings.schedule.hour)
     end
+    # This job should only run once
+    sidekiq_options({
+      unique: true
+    })
 
     def initialize
       @synchronizer = EverbridgeSync::Synchronizer.new
