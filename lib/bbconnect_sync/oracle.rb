a037b8004a963dbafee47b2e7827feb73d168b56
Adam Crownoble <adam.crownoble@biola.edu>
2014-07-10 15:46:19 -0700

    Use new bgv_bbconnect view

--- lib/bbconnect_sync/oracle.rb
+++ lib/bbconnect_sync/oracle.rb
@@ -4,13 +4,13 @@ module BBConnectSync
   class Oracle
     def contacts
       BBConnect::ContactCollection.new.tap do |collection|
-        # BGV_EVERBRIDGE returns multiple rows for a person who is a student and employee, so we are concatenating the two rows
-        cursor = connection.exec("SELECT ID, FNAME, LNAME, CELL, EMAIL, listagg( AFFILIATION, '|' ) WITHIN GROUP( order by AFFILIATION ) AS AFFILIATIONS, HOUSING_1
-                                  FROM BGV_EVERBRIDGE
-                                  GROUP BY ID, FNAME, LNAME, CELL, EMAIL, HOUSING_1")
+        # BGV_BBCONNECT returns multiple rows for a person who is a student and employee, so we are concatenating the two rows
+        cursor = connection.exec("SELECT ID, PNAME, LNAME, CELL, EMAIL, listagg( AFFILIATION, '|' ) WITHIN GROUP( order by AFFILIATION ) AS AFFILIATIONS, HOUSING_1
+                                  FROM BGV_BBCONNECT
+                                  GROUP BY ID, PNAME, LNAME, CELL, EMAIL, HOUSING_1")
         while row = cursor.fetch_hash
           id_number   = row['ID']
-          first_name  = row['FNAME']
+          first_name  = row['PNAME']
           last_name   = row['LNAME']
           email       = row['EMAIL']
           cell        = row['CELL']
