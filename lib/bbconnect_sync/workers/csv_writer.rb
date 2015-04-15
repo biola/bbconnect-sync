c9fe854cd876f8ac172baf347468553febb41a3e
Adam Crownoble <adam.crownoble@biola.edu>
2015-04-15 09:21:40 -0700

    Add comment about running worker synchronously

--- lib/bbconnect_sync/workers/csv_writer.rb
+++ lib/bbconnect_sync/workers/csv_writer.rb
@@ -17,6 +17,9 @@ module BBConnectSync
       def perform
         if Settings.worker.enabled
           if file_path = @synchronizer.sync!
+            # It would be nice to run this asynchronously but in a load balanced situation
+            # we can't guarantee that the file wasn't written onto a different server.
+            # Until we have a shared files directory, this is the best workaround.
             CSVUploader.new.perform(file_path)
           end
         end
